cda6d2b0583b58fbc5baa52a9d89a5740f558438
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-08-07 12:00:35 +0700

    update  delete user

--- src/router/userRourter.ts
+++ src/router/userRourter.ts
@@ -59,6 +59,9 @@ router.post(`/`, async (req, res) => {
             email: user.email,
             firstName: user.firstName,
             lastName: user.lastName,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+            role: user.role,
           },
         });
       } else {
@@ -80,7 +83,6 @@ router.post(`/`, async (req, res) => {
 
 router.post(`/register`, async (req, res) => {
   const { email, password, firstName, lastName, role } = req.body;
-  console.log(role);
 
   try {
     await userSchema.validate({ email, password, firstName, lastName, role });
@@ -122,4 +124,84 @@ router.post(`/register`, async (req, res) => {
     });
   }
 });
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const userEdit = await userSchema.findOne({ _id: id });
+  const user = await userSchema.findOne({ email: req.headers['gmail_user'] });
+  if (
+    userEdit !== null &&
+    user !== null &&
+    userEdit?.email !== user?.email &&
+    ((user.role?.includes('superadmin') && !userEdit?.role?.includes('superadmin')) ||
+      (user?.role?.includes('admin') && (!userEdit?.role?.includes('admin') || !userEdit.role?.includes('superadmin'))))
+  ) {
+    await userSchema
+      .findByIdAndDelete(id)
+      .catch((err) => {
+        return res.status(500).json({
+          success: false,
+          message: 'The system crashed, please try again in a moment',
+        });
+      })
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          message: 'You have successfully deleted the user',
+        });
+      });
+  } else {
+    return res.status(400).json({
+      message: 'You are not authorized to delete this user',
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { email, firstName, lastName, created_at, updated_at, role } = req.body;
+  const userEdit = await userSchema.findOne({ _id: id });
+  const user = await userSchema.findOne({ email: req.headers['gmail_user'] });
+
+  if (
+    userEdit !== null &&
+    user !== null &&
+    ((user.role?.includes('superadmin') && !userEdit?.role?.includes('superadmin')) ||
+      (user?.role?.includes('admin') && (!userEdit?.role?.includes('admin') || !userEdit.role?.includes('superadmin'))))
+  ) {
+    const user = await userSchema.findOne({ email });
+    if (user.email !== userEdit.email) {
+      return res.status(400).json({
+        message: 'Email already exists',
+      });
+    } else {
+      await userSchema
+        .findByIdAndUpdate(id, {
+          email,
+          firstName,
+          lastName,
+          created_at,
+          updated_at,
+          role,
+        })
+        .catch((err) => {
+          return res.status(500).json({
+            success: false,
+            message: 'The system crashed, please try again in a moment',
+          });
+        })
+        .then(() => {
+          return res.status(200).json({
+            success: true,
+            message: 'You have successfully edit the user',
+          });
+        });
+    }
+  } else {
+    return res.status(400).json({
+      message: 'You are not authorized to edit this user',
+    });
+  }
+});
+
 export default router;
